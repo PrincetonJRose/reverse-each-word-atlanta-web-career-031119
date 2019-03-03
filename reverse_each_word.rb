a1e8a7807fbd9934e6f66709c678758688698a53
Princeton Rose <decoyzpr@gmail.com>
2019-03-03 03:52:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(phrase)
   phrase = phrase.split(' ')
   phrase.each { |index| phrase[index] = phrase[index].reverse }
   phrase.join.to_s
-  return phrase2
+  return phrase
 
 end
